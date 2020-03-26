01fb55d370692a58d03b6bd1997b2ab25d48650b
Auxence <auxence.blondel@gmail.com>
2020-03-26 01:12:46 +0100

    change append to prepend and speaker position

--- script.js
+++ script.js
@@ -6,42 +6,52 @@ recognition.interimResults = true; //décline chaque mot en un resultat
 const speakers = ['Abdou', 'Auxence', 'Émilie', 'marjolaine', 'Mohamed', 'Walid', 'Aurélien', 'Margot'];
 
 let p = document.createElement('p');
-let speakerName = document.createElement("h3");
+let heure = "";
+let speakerName = document.getElementById("speakerName");
 const words = document.querySelector('.words');
 
+// let valueScroll = 0;
+
+
 function addSpeaker(transcript){
+
     for(let speaker of speakers){
-        if(transcript.includes(speaker))
-            return speaker + ":";
-    }  
+        if(transcript.includes(`c'est ${speaker} qui parle`))
+            return ` ${speaker}`;  
+}
 }
 
-words.appendChild(speakerName);
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-    
+
+    //Scroll auto
+    // if(words.clientHeight >= 700 - 100){
+    //     window.scrollTo(0, valueScroll); 
+    //     valueScroll += 50;
+    // }
+
     const transcript = Array.from(e.results) //on créer un tableau selon les resultats
         .map(result => result[0]) //stock dans le tableau le resultat à la place [0]
         .map(result => result.transcript) // cherche le paramètre transcript pour le stocker dans le tableau
         .join(''); // join toutes les cases du tableau
-    console.log(transcript);
-
-    speakerName.textContent = addSpeaker(transcript);
-    p.textContent = transcript;
+    
+    addSpeaker(transcript) === undefined ? speakerName : speakerName.textContent = addSpeaker(transcript)
+        
+    date = new Date;
+    
+    p.innerHTML = `<span>${date.getHours()}:${date.getMinutes()}</span> ${transcript}`;
 
     if(transcript.includes("red") || transcript.includes("rouge"))
         document.querySelector("body").style.backgroundColor = "red";
 
     if(e.results[0].isFinal){ // verifie si on est à la fin du tableau global
         p = document.createElement("p");
-        speakerName = document.createElement("h3")
-        words.appendChild(speakerName);
-        words.appendChild(p);
-
+        words.prepend(p);
     }
 });
 
 recognition.addEventListener('end', recognition.start); // rappel la function start quand on arrête de parler
 
 recognition.start();
+
